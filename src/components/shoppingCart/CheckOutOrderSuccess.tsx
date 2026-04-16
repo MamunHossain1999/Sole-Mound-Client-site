e8dd7e3bb5adaa5041fafd86988f5e5ca9bcaeb9
Your Name <youremail@example.com>
2026-04-16 22:48:39 +0600

    check out success page

---
+++ src/components/shoppingCart/CheckOutOrderSuccess.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CheckOutOrderSuccess = () => {
+    return (
+        <div>
+            hoidkdfd
+        </div>
+    );
+};
+
+export default CheckOutOrderSuccess;
